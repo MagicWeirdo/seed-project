8c07c67d5e1df6a97f6d1d6e70ca63f4907cc8e5
Oliver Hsu <mark_rhett@yahoo.com>
2017-06-16 11:37:06 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const catalysis = require('catalysis');
+
+catalysis.bootstrap({
+  basePath: __dirname
+});
---
+++ config/setting.js
@@ -0,0 +1,18 @@
+module.exports = {
+  hostname: '127.0.0.1',
+  port: 80,
+  static_root: '/static',
+  enable_websocket: false,
+  middlewares: [
+
+  ],
+  actions: [
+
+  ],
+  models: [
+
+  ],
+  services: [
+
+  ]
+};
